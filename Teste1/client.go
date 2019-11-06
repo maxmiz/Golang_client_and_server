945648237a691a4bd1569f8732c4dfd9b0338ba4
Rafael <maxmiz>
2019-11-06 12:48:07 -0300

    Ta no caminho

--- Teste1/client.go
+++ Teste1/client.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Println("teste")
+
+	conn, _ := net.Dial("tcp", "127.0.0.1:8081")
+	for {
+
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Text to send: ")
+		//Cliente
+		text, _ := reader.ReadString('\n')
+		// send to socket
+		fmt.Fprintf(conn, "Client: \n")
+
+		//Mensagem
+		text, _ = reader.ReadString('\n')
+		// send to socket
+		fmt.Fprintf(conn, text+"\n")
+
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message from server: " + message)
+	}
+
+}
+
+func send_message(r *reader) {
+	text, _ := r.ReadString('\n')
+	fmt.Fprintf()
 }
--- Teste1/server.go
+++ Teste1/server.go
@@ -1,5 +1,31 @@
 package main
 
-func main2() {
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
 
+func main() {
+
+	fmt.Println("Launching server...")
+
+	// listen on all interfaces
+	ln, _ := net.Listen("tcp", ":8081")
+
+	// accept connection on port
+	conn, _ := ln.Accept()
+
+	// run loop forever (or until ctrl-c)
+	for {
+
+		//Nome do cliente
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Client:", string(message))
+		//Mensagem do cliente
+		message, _ = bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message Received:", string(message))
+
+		conn.Write([]byte("OK\n"))
+	}
 }
